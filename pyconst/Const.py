4e4ff36234cc0502abafaed077c401530c30c31f
Zachery Thomas (zacthoma) <zacthoma@cisco.com>
2018-12-10 21:50:18 -0500

    Added pickle support. Made objList manditory

--- pyconst/Const.py
+++ pyconst/Const.py
@@ -1,24 +1,22 @@
 import _thread
 import time
+import pickle
+
 class Const:
-    def __init__(self, objList=None):
-        if objList is not None:
-            self.objList = objList
-            pass
-        else:
-            self.objList = []
+    def __init__(self, objList):
+        self.objList = objList
+
+        objListConstCopy = [pickle.loads(pickle.dumps(obj)) for obj in objList]
+        self.objListConstCopy = objListConstCopy
 
         _thread.start_new_thread(self.run , ())
 
-    def add_obj(self, obj):
-        self.objList.append(obj)
 
     def run(self):
         # do the thing
         while True:
-            time.sleep(1)
+            time.sleep(10)
             print("running...") #TODO: remove
+            print( pickle.dumps(self.objList) == pickle.dumps(self.objListConstCopy) )
+
             pass
-        
-        
-        

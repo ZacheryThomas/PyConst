ee4d7195b3c840191947a2dc62af5c33aa030dfb
Zachery Thomas (zacthoma) <zacthoma@cisco.com>
2019-01-03 10:51:16 -0500

    Here's some junk

--- pyconst/Const.py
+++ pyconst/Const.py
@@ -4,6 +4,9 @@ import pickle
 
 class Const:
     def __init__(self, objList, shame="Shame on you for try to change my object"):
+        print('----')
+        print('classobj:', id(objList))
+        print('----')
         self.objList = objList
         self.shame = shame
 
@@ -16,8 +19,18 @@ class Const:
     def run(self):
         # do the thing
         while True:
-            time.sleep(10)
-            print(shame) #TODO: only print when the objects differ
-            print( pickle.dumps(self.objList) == pickle.dumps(self.objListConstCopy) )
+            # time.sleep(10)
+            #print(self.objList)
+            #print(self.shame) #TODO: only print when the objects differ
+            #print( pickle.dumps(self.objList) == pickle.dumps(self.objListConstCopy) )
+
+            for index, enum in enumerate(self.objList):
+                # print(self.objList[index], self.objListConstCopy[index])
+                if pickle.dumps(self.objList[index]) != pickle.dumps(self.objListConstCopy[index]):
+                    # print('----')
+                    print(self.objList[index],self.objListConstCopy[index])
+                    #print('----')
+                    self.objList[index] = pickle.loads(pickle.dumps(self.objListConstCopy[index]))
+                    print(self.objList[index],self.objListConstCopy[index])
 
             pass

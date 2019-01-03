883d9d0f9187c496fbefb3367fcbde6969dbdb92
Jeremiah Gelb <jmgelb@ncsu.edu>
2019-01-03 11:33:10 -0500

    use dill instead of pickle. Remove many debug prints

--- pyconst/Const.py
+++ pyconst/Const.py
@@ -1,36 +1,20 @@
 import _thread
-import time
-import pickle
+import dill
 
 class Const:
     def __init__(self, objList, shame="Shame on you for try to change my object"):
-        print('----')
-        print('classobj:', id(objList))
-        print('----')
+
         self.objList = objList
         self.shame = shame
 
-        objListConstCopy = [pickle.loads(pickle.dumps(obj)) for obj in objList]
-        self.objListConstCopy = objListConstCopy
+        self.dump = dill.dumps(objList)
 
         _thread.start_new_thread(self.run , ())
 
-
     def run(self):
-        # do the thing
         while True:
-            # time.sleep(10)
-            #print(self.objList)
-            #print(self.shame) #TODO: only print when the objects differ
-            #print( pickle.dumps(self.objList) == pickle.dumps(self.objListConstCopy) )
-
-            for index, enum in enumerate(self.objList):
-                # print(self.objList[index], self.objListConstCopy[index])
-                if pickle.dumps(self.objList[index]) != pickle.dumps(self.objListConstCopy[index]):
-                    # print('----')
-                    print(self.objList[index],self.objListConstCopy[index])
-                    #print('----')
-                    self.objList[index] = pickle.loads(pickle.dumps(self.objListConstCopy[index]))
-                    print(self.objList[index],self.objListConstCopy[index])
-
-            pass
+            if dill.dumps(self.objList) != self.dump:
+                print(self.shame)
+                load = dill.loads(self.dump)
+                for index, obj in enumerate(self.objList):                       
+                        self.objList[index] = load[index]
